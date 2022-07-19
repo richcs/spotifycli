712c1169d17f456d111a495fed87b61c4a2119cb
Richard Lei <46201873+richcs@users.noreply.github.com>
2022-07-18 21:13:55 -0400

    fetch data from spotify api

---
+++ src/config.rs
@@ -0,0 +1,3 @@
+pub const CLIENT_ID: &str = "a46e4576183f4cdea53ff1c559deb4a9";
+pub const PATH_STRING:&str = "C:/ProgramData/spotifycli"; // TODO: CHANGE THIS
+pub const CREDENTIALS_FILE:&str = "/credentials.json";
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-use std::io::{self, Write, Read, stdin};
+use std::io::{self, Write};
 use std::path::Path;
+use std::process::exit;
+use librespot::core::keymaster;
 use text_io::read;
 use rpassword::read_password;
 use librespot::{core::{config::SessionConfig, session::Session, cache::Cache}, discovery::Credentials};
 
-static PATH_STRING:&str = "C:/ProgramData/spotifycli"; // TODO: CHANGE THIS
-static CREDENTIALS_FILE:&str = "/credentials.json";
+mod config;
+mod fetcher;
+
+const SCOPES: &str =
+    "streaming,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-read-private";
 
 #[tokio::main]
 async fn main() {
-    login_user_pass();
-    let path = Path::new(PATH_STRING);
+    let path = Path::new(config::PATH_STRING);
     let cache = Cache::new(Some(path), None, None, None).ok();
     let credentials = get_credentials(&cache);
     let session_config = SessionConfig::default();
-    let (session, _) = Session::connect(session_config, credentials, cache, true).await.unwrap();
-    println!("Connected Successfully");
+    let connect_result = Session::connect(session_config, credentials, cache, true).await;
+    match connect_result {
+        Result::Err(_) => { 
+            println!("Login Failed");
+            exit(0);
+        },
+        Result::Ok((session, _)) => {
+            let token = keymaster::get_token(&session, config::CLIENT_ID, SCOPES).await.unwrap();
+            let spot_fetcher = fetcher::Fetcher::new();
+            let playlists = spot_fetcher.get_playlists(token).await.unwrap();
+            println!("Connected Successfully");
+        }
+    }
+
+    // Retrieve data from spotify
 
     // Start accepting commands
 }
 
 fn get_credentials(cache : &Option<Cache>) -> Credentials {
-    let credential_path_string = PATH_STRING.to_owned() + CREDENTIALS_FILE;
+    let credential_path_string = config::PATH_STRING.to_owned() + config::CREDENTIALS_FILE;
     if !Path::new(credential_path_string.as_str()).exists() {
         return login_user_pass();
     }
@@ -36,7 +53,6 @@ fn login_user_pass() -> Credentials {
     print!("Enter Username: ");
     io::stdout().flush().unwrap();
     let username : String = read!("{}\n");
-    println!("This is your username: {}", username);
     print!("Enter Password: ");
     io::stdout().flush().unwrap();
     let password : String = read_password().unwrap();
